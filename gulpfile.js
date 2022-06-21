b79a981453df61c9df5e12bc73c0a75bba72f111
Adkhambek <adkhambekyusupov@gmail.com>
2022-06-21 21:24:39 +0500

    build

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,75 @@
+
+// const include = require('gulp-file-include')
+// const htmlmin = require('gulp-htmlmin')
+// const sync = require('browser-sync').create()
 const { src, dest, series, watch } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 const csso = require('gulp-csso')
-const include = require('gulp-file-include')
-const htmlmin = require('gulp-htmlmin')
 const autoprefixer = require('gulp-autoprefixer')
 const concat = require('gulp-concat')
 const del =require('del');
-const sync = require('browser-sync').create()
 
-function html() {
-  return src('src/**.html')
-    .pipe(include({
-      prefix: '@@'
-    }))
-    .pipe(htmlmin({
-      collapseWhitespace: true
-    }))
-    .pipe(dest('build'))
+const paths = {
+  styles: {
+    src: 'src/scss/**/*.scss',
+    dest: 'build/css/'
+  },
+  script: {
+    src: 'src/js/**/*.js',
+    dest: 'build/js/'
+  }
 }
 
-function scss() {
-  return src('src/scss/**.scss')
+
+function styles() {
+  return src(paths.styles.src)
       .pipe(sass())
       .pipe(autoprefixer({
         browsers: [ 'last 2 version' ]
       }))
       .pipe(csso())
-      .pipe(concat('index.css'))
-      .pipe(dest('build'))
-}
+      .pipe(concat('main.min.css'))
+      .pipe(dest(paths.styles.dest))
+    }
 
-function clear() {
-  return del('build')
-}
+    function clear() {
+      return del(['build'])
+    }
 
-function serve() {
-  sync.init({
-    server: './build'
-  })
-  // @ts-ignore
-  watch('src/**.html', series(html)).on('change', sync.reload)
-  // @ts-ignore
-  watch('src/scss/**.scss', series(scss)).on('change', sync.reload)
-}
+    function watching() {
+      watch(paths.styles.src, styles)
+    }
 
-exports.build = series(clear, scss, html)
-exports.serve = series(clear, scss, html, serve)
-exports.clear = clear
+
+    const start = series(clear, styles, watching)
+    exports.styles = styles
+    exports.clear = clear
+    exports.start = start
+    exports.default = start
+
+
+// function html() {
+//   return src('src/**.html')
+//     .pipe(include({
+//       prefix: '@@'
+//     }))
+//     .pipe(htmlmin({
+//       collapseWhitespace: true
+//     }))
+//     .pipe(dest('build'))
+// }
+
+
+
+// function serve() {
+//   sync.init({
+//     server: './build'
+//   })
+//   // @ts-ignore
+//   watch('src/**.html', series(html)).on('change', sync.reload)
+//   // @ts-ignore
+//   watch('src/scss/**.scss', series(scss)).on('change', sync.reload)
+// }
+
+// exports.build = series(clear, styles, html)
+// exports.serve = series(clear, styles, html, serve)
