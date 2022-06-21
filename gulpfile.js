c08d3380c31fdc64b60e63faca75e07b23d5fb37
Adkhambek <adkhambekyusupov@gmail.com>
2022-06-22 02:49:29 +0500

    finish

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,173 @@
-
-// const include = require('gulp-file-include')
-// const htmlmin = require('gulp-htmlmin')
-// const sync = require('browser-sync').create()
-const { src, dest, series, watch } = require('gulp')
+const { src, dest, series, watch, parallel } = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const include = require('gulp-file-include');
+const babel = require('gulp-babel');
+const csso = require('gulp-csso');
 const sass = require('gulp-sass')(require('sass'));
-const csso = require('gulp-csso')
-const autoprefixer = require('gulp-autoprefixer')
-const concat = require('gulp-concat')
-const del =require('del');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+var newer = require('gulp-newer');
+const size = require('gulp-size');
+const browsersync = require('browser-sync').create();
+const gulppug = require('gulp-pug');
+const del = require('del');
 
 const paths = {
+  pug: {
+    src: 'src/*.pug',
+    dest: 'build/',
+  },
+  html: {
+    src: 'src/*.html',
+    dest: 'build/',
+  },
   styles: {
     src: 'src/scss/**/*.scss',
-    dest: 'build/css/'
+    dest: 'build/css/',
   },
-  script: {
+  scripts: {
     src: 'src/js/**/*.js',
-    dest: 'build/js/'
-  }
+    dest: 'build/js/',
+  },
+  images: {
+    src: 'src/images/**/*',
+    dest: 'build/media/',
+  },
+};
+
+function pug() {
+  return src(paths.pug.src)
+    .pipe(gulppug())
+    .pipe(dest(paths.pug.dest))
+    .pipe(browsersync.stream());
 }
 
+function html() {
+  return src(paths.html.src)
+    .pipe(
+      include({
+        prefix: '@@',
+      }),
+    )
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      }),
+    )
+    .pipe(dest(paths.html.dest))
+    .pipe(browsersync.stream());
+}
 
 function styles() {
   return src(paths.styles.src)
-      .pipe(sass())
-      .pipe(autoprefixer({
-        browsers: [ 'last 2 version' ]
-      }))
-      .pipe(csso())
-      .pipe(concat('main.min.css'))
-      .pipe(dest(paths.styles.dest))
-    }
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 version'],
+        cascade: false,
+      }),
+    )
+    .pipe(csso())
+    .pipe(concat('main.min.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest(paths.styles.dest))
+    .pipe(browsersync.stream());
+}
 
-    function clear() {
-      return del(['build'])
-    }
+function scripts() {
+  return src(paths.scripts.src)
+    .pipe(sourcemaps.init())
+    .pipe(
+      babel({
+        presets: ['@babel/env'],
+      }),
+    )
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest(paths.scripts.dest))
+    .pipe(browsersync.stream());
+}
 
-    function watching() {
-      watch(paths.styles.src, styles)
-    }
+async function images() {
+  const imagemin = await import('gulp-imagemin');
+  const imagePlugins = [
+    imagemin.mozjpeg({
+      progressive: true,
+    }),
+    imagemin.optipng({
+      optimizationLevel: 5,
+    }),
+  ];
 
+  return src(paths.images.src)
+    .pipe(newer(paths.images.dest))
+    .pipe(imagemin.default(imagePlugins))
+    .pipe(dest(paths.images.dest));
+}
 
-    const start = series(clear, styles, watching)
-    exports.styles = styles
-    exports.clear = clear
-    exports.start = start
-    exports.default = start
+function clear() {
+  return del(['build/*', '!build/media']);
+}
+function clearFull() {
+  return del(['build']);
+}
 
+function buildSize() {
+  return src('build/**/*').pipe(
+    size({
+      showTotal: true,
+    }),
+  );
+}
+function srcSize() {
+  return src('src/**/*').pipe(
+    size({
+      showTotal: true,
+    }),
+  );
+}
 
-// function html() {
-//   return src('src/**.html')
-//     .pipe(include({
-//       prefix: '@@'
-//     }))
-//     .pipe(htmlmin({
-//       collapseWhitespace: true
-//     }))
-//     .pipe(dest('build'))
-// }
+function watching() {
+  browsersync.init({
+    server: {
+      baseDir: './build/',
+    },
+  });
+  watch(paths.html.dest).on('change', browsersync.reload);
+  watch(paths.html.src, html);
+  watch(paths.styles.src, styles);
+  watch(paths.scripts.src, scripts);
+  watch(paths.images.src, images);
+}
 
+const start = series(
+  clear,
+  html,
+  parallel(styles, scripts, images),
+  srcSize,
+  buildSize,
+  watching,
+);
+const build = series(
+  clearFull,
+  html,
+  parallel(styles, scripts, images),
+  srcSize,
+  buildSize,
+);
 
-
-// function serve() {
-//   sync.init({
-//     server: './build'
-//   })
-//   // @ts-ignore
-//   watch('src/**.html', series(html)).on('change', sync.reload)
-//   // @ts-ignore
-//   watch('src/scss/**.scss', series(scss)).on('change', sync.reload)
-// }
-
-// exports.build = series(clear, styles, html)
-// exports.serve = series(clear, styles, html, serve)
+// Gulp command line
+exports.pug = pug;
+exports.html = html;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.images = images;
+exports.clear = clear;
+exports.clearfull = clearFull;
+exports.default = start;
+exports.build = build;
+exports.start = start;
